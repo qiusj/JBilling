51ebcd48725b9ad7cd85f819e093c024431eff9e
Krzysztof Kwiatkowski <krzysztof.kwiatkowski@morecratf.pl>
2017-09-11 18:58:00 +0200

    init

--- jbilling-community-4.1.1/grails-app/domain/jbilling/RecentItemType.groovy
+++ jbilling-community-4.1.1/grails-app/domain/jbilling/RecentItemType.groovy
@@ -22,20 +22,21 @@ package jbilling
 
 /**
  * RecentItemType
- 
+
  * @author Brian Cowdery
  * @since  07-12-2010
  */
 enum RecentItemType {
 
     INVOICE             ("invoice", "list", null, "icon09.gif", "recent.item.invoice.title"),
-    ORDER               ("order", "list", null, "icon10.gif", "recent.item.order.title"),
+    ORDER               ("order", "showListAndOrder", null, "icon10.gif", "recent.item.order.title"),
     PRODUCT             ("product", "show", null, "icon13.gif", "recent.item.product.title"),
     CUSTOMER            ("customer", "list", null, "icon12.gif", "recent.item.customer.title"),
     PAYMENT             ("payment", "list", null, "icon11.gif", "recent.item.payment.title"),
     PLUGIN              ("plugin", "list", null, "icon13.gif", "recent.item.plugin.title"),
     BILLINGPROCESS      ("billing", "show", null, "icon13.gif", "recent.item.billing.process.title"),
-	
+    MEDIATIONPROCESS    ("mediation", "show", null, "icon13.gif", "recent.item.mediation.process.title");
+
     String controller
     String action
     Map params
